e377bc7bd437fb3ab6672a724c24ec2b2047f13f
moncef <maktimoncef@gmail.com>
2024-03-07 10:48:04 +0100

    first commit

---
+++ myapp/api/url.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView  # new
+
+from myapp.api.views import UserCreateAPIView, RetrieveUpdateAPIView, LogInView
+
+urlpatterns = [
+    path('users/', UserCreateAPIView.as_view(), name='user-create'),
+    path('users/<uuid:pk>/', RetrieveUpdateAPIView.as_view(), name='user-update'),
+    path('login/', LogInView.as_view(), name='log_in'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns = urlpatterns + router.urls
